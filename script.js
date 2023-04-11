11bb13789b5def8d057c781dff6c5e3837ff0198
nurakbarova <nurayakbarova76@gmail.com>
2023-04-11 23:14:51 +0400

    object

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+// TASK2
+class product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        // product(FatPercent)
+        // this.FatPercent = FatPercent;
+        // AvgDeath()
+        // return(name+price)/2
+
+    }
+}
+const product1 = new product("milla", 2);
+    // Object.setPrototypeOf(stu1,person5);
+
+console.log(product1);
+// const FatPercent1 =  new FatPercent("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Product{
+//     constructor(name,price){
+//         this.name=name;
+//         this.price=price;
+//     }
+// }
